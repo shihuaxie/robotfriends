c8759357c5dd560d9299bc2c042fd1a8cf12f13b
Sylvia Xie <100256628+SYLVIAXIE1997@users.noreply.github.com>
2022-10-28 17:33:13 +1100

    use hooks instead of classes

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,39 +1,45 @@
-import React, {Component} from 'react';
+import React, {useState, useEffect} from 'react';
 import CardList from "../components/CardList";
 import Scroll from '../components/Scroll';
 import SearchBox from '../components/SearchBox';
 import ErrorBoundry from '../components/ErrorBoundry'
 import './App.css';
 
-export default class App extends Component {
-    constructor() {
-        super()
-        this.state = {
-            robots:[],
-            searchfield: ''
-        }
-    }
+export default function App () {
 
-componentDidMount() {
-        fetch('https://jsonplaceholder.typicode.com/users')
+ //useState()will return us an array, we put the initial state of robots in the bracket  
+ //robots represents the initial state of robots; setRobots is the function that changes robots;
+    const[robots, setRobots] = useState([])
+    const [searchfield, setSearchField] = useState('')
+    const[count, setCount] =useState(0)
+
+// when app runs, it will run what inside in the useEffect, fetch is the first param
+useEffect(()=>{
+            fetch('https://jsonplaceholder.typicode.com/users')
             .then(response=>response.json())
-            .then(users=> this.setState({robots:users}))
-}
+            .then(users=> {setRobots(users)})
+            console.log(count);
+}, [count]) //only run if count changes
+//the second param is an array, we keep it empty here means just run useEffect initially when App renders
+//component shortcut is the empty array here
 
-    onSearchChange = (event) => {
+
+    const onSearchChange = (event) => {
         // console.log(event)
-        this.setState({searchfield: event.target.value})
+        setSearchField(event.target.value)
     }
 
-    render() {
-        const{robots, searchfield} = this.state
         const filteredRobots = robots.filter(robot => {
             return robot.name.toLowerCase().includes(searchfield.toLowerCase())
         })
-        return (
+
+        return !robots.length?
+        <h1>loading...</h1> :
+         (
             <div className='tc'>
                 <h1 className='f1'>RobotFriends</h1>
-                <SearchBox searchChange={this.onSearchChange}/>
+                <button onClick={()=>setCount(count+1)}>click me!</button>
+                <SearchBox searchChange={onSearchChange}/>
                <Scroll>
                    <ErrorBoundry>
                        <CardList robots={filteredRobots}/>
@@ -43,4 +49,4 @@ componentDidMount() {
             </div>
         );
     }
-}
+
